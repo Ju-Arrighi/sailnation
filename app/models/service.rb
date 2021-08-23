7671001d6f1d9313834e39343c870d3df7bb1757
Juliana Arrighi <jarrighi@poli.ufrj.br>
2021-08-23 18:36:07 -0300

    fixing heroku deploy

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,8 @@
 class Service < ApplicationRecord
   belongs_to :user
   has_many :reviews, denpendency: :destroy
-  validates :video_url, :description, :part, :brand, presence: true
+  validates :video_url, presence: true
+  validates :description, presence: true
+  validates :part, presence: true
+  validates :brand, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :services
   has_many :reviews
-  validates :first_name, :last_name, :email, :username, presence: true
-  validates :username, :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true, uniqueness: true
 end
