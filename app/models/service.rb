3f7df659a3ae0e9047bb64878dc8cae2c0f6c72e
Juliana Arrighi <jarrighi@poli.ufrj.br>
2021-08-23 19:10:39 -0300

    fixing erros

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,6 +1,6 @@
 class Service < ApplicationRecord
   belongs_to :user
-  has_many :reviews, dependency: :destroy
+  has_many :reviews, dependent: :destroy
   validates :video_url, presence: true
   validates :description, presence: true
   validates :part, presence: true
