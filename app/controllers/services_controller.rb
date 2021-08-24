f9d554bb418f2637afeed47968e9b308a6d543da
Juliana Arrighi <jarrighi@poli.ufrj.br>
2021-08-24 19:55:32 -0300

    #index methods done, search bar without button

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,21 +1,30 @@
 class ServicesController < ApplicationController
-  def create
-    @restaurant = Restaurant.new(params[:restaurant])
-    @restaurant.save
-  end
-
   def index
     @services = Service.all
   end
 
+  def new
+    @services = Service.new
+  end
+
+  def create
+    @services = Service.new(params[:services])
+    @services.save
+    if @services.save
+      redirect_to services_path(@services)
+    else
+      render :new
+    end
+  end
+
   def show
-    @restaurant = Restaurant.find(params[:id])
+    @services = Service.find(params[:id])
   end
 
   private
 
-  def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :rating)
+  def service_params
+    params.require(:services).permit(:brand, :part, :description, :level, :video_url)
   end
 
 end
