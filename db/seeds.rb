5ef386c40743802d2da277cafef0f8c17d2d5dad
Juliana Arrighi <jarrighi@poli.ufrj.br>
2021-08-25 15:42:09 -0300

    fixed errors on seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts 'Cleaning up database..'
+Service.destroy_all
 User.destroy_all
-# Service.destroy_all
+
 # Review.destroy_all
 
 puts 'Creating Users..'
-User.create(first_name:'Yuhri', last_name:'Parada', email:'yuhriparada@hotmail.com', password:'abc123', phone_num: 9999-9999, bio: 'Hey', location: 'Rio de Janeiro')
-User.create(first_name:'Juliana', last_name:'Arrighi', email:'jarrighi@poli.ufrj.br', password:'abc123', phone_num: 9999-9999, bio: 'Hey', location: 'Rio de Janeiro')
-User.create(first_name:'Justin', last_name:'Knox', email:'techbiotic@icloud.com', password:'abc123', phone_num: 9999-9999, bio: 'Hey', location: 'Rio de Janeiro')
+User.create!(first_name:'Yuhri', last_name:'Parada', email:'yuhriparada@hotmail.com', password:'abc123', phone_num: 9999-9999, bio: 'Hey', location: 'Rio de Janeiro')
+ju = User.create!(first_name:'Juliana', last_name:'Arrighi', email:'jarrighi@poli.ufrj.br', password:'abc123', phone_num: 9999-9999, bio: 'Hey', location: 'Rio de Janeiro')
+User.create!(first_name:'Justin', last_name:'Knox', email:'techbiotic@icloud.com', password:'abc123', phone_num: 9999-9999, bio: 'Hey', location: 'Rio de Janeiro')
 puts 'All Done !'
 
-Service.create( brand: "Cool boat brand", part: "Engine block", description: "These is the text field for the database", level: 3, video_url: "http://youtube.com", user_id: 4, created_at: null, updated_at: null)
+
+service1 = Service.new(brand: "Cool boat brand", part: "Engine block", description: "These is the text field for the database", level: "easy", video_url: "http://youtube.com")
+service1.user = ju
+service1.save!
+puts "worked!"
