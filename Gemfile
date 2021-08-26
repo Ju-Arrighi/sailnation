36f5d8e7353ae643fa5cde4ba5f23a4429fd8dff
Juliana Arrighi <jarrighi@poli.ufrj.br>
2021-08-26 19:26:25 -0300

    geocoding filling db with lat and long

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 
 gem 'faker'
+
+gem 'geocoder'
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :reviews
   validates :first_name, presence: true
   validates :last_name, presence: true
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
