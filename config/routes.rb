4015241eb2164d7f9aa0d01b0e4a563a5cc9addb
Justin Knox <techbiotic@posteo.de>
2021-08-26 12:46:52 -0300

    first commit on working show page, controller, model, and routes completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
     resources :services, only: [:index, :new, :create, :show]
+
+    resources :users, only: [:show]
+
+    # get 'dashboard' to: "pages#user"
 end
